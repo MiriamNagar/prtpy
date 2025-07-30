5e402d2aaa6a362577dda3b90c07045618d8a51d
MiriamNagar <118976833+MiriamNagar@users.noreply.github.com>
2025-07-31 00:08:15 +0300

    fixing logger use n triplet_planner.py

--- prtpy/packing/triplet_algo/triplet_planner.py
+++ prtpy/packing/triplet_algo/triplet_planner.py
@@ -10,7 +10,7 @@ from .models import SolverData, TripletSearchContext
 from .triplet_backtracker import TripletBacktracker
 from .triplet_local_search import TripletLocalSearch
 
-logger = logging.getLogger("trialgo.triplet_algo")
+logger = logging.getLogger(__name__)
 
 
 class TripletPlanner:
